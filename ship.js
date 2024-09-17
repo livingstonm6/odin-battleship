99cd1a1dd721710f9750dd3caeb6c5e6bcf49d70
livingstonm6 <livingstonm6@mymacewan.ca>
2024-09-17 16:48:18 -0600

    Implemented Ship Factory.

---
+++ ship.js
@@ -0,0 +1,19 @@
+const shipFactory = (() => {
+    const createShip = function (length) {
+        const len = length;
+        let numHits = 0;
+
+        const hit = function(){
+            numHits++;
+        }
+
+        const isSunk = function(){
+            return numHits === len;
+        }
+
+        return {len, numHits, hit, isSunk};
+    }
+
+
+    return {createShip};
+})();
