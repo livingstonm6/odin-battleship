f14cae20e1e84165751db2d2c2617e072d6f66a4
livingstonm6 <livingstonm6@mymacewan.ca>
2024-09-21 19:31:01 -0600

    Implemented the ability to start a game.
    Also added basic CSS styling.

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+import uiManager from './ui.js';
+import gameManager from './game.js';
+const status = document.querySelector(".status");
+
+function displayGrids () {
+    const grids = gameManager.getCurrentGrids();
+    uiManager.displayShipBoard(grids.shipGrid);
+    uiManager.displayShotBoard(grids.shotGrid);
+    const currentPlayer = gameManager.getCurrentPlayer();
+    status.textContent = "Player " + (currentPlayer + 1).toString() + "'s turn";
+}
+
+function initUIElements (){
+    const startButton = document.querySelector(".start-button");
+    startButton.addEventListener("click", () => {
+        gameManager.start(false);
+        displayGrids();
+    });
+}
+
+
+initUIElements();
+
+
